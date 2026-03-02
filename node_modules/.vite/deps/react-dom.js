0c0c91dc2ffa530d1ff6543599c013601a978de0
Warren <warrendsteinacker@gmail.com>
2026-03-02 10:54:33 -0500

    making commit

---
+++ node_modules/.vite/deps/react-dom.js
@@ -0,0 +1,6 @@
+import {
+  require_react_dom
+} from "./chunk-74IYAFRW.js";
+import "./chunk-S77QC6AN.js";
+import "./chunk-HKJ2B2AA.js";
+export default require_react_dom();
---
+++ node_modules/.vite/deps/react.js
@@ -0,0 +1,5 @@
+import {
+  require_react
+} from "./chunk-S77QC6AN.js";
+import "./chunk-HKJ2B2AA.js";
+export default require_react();
