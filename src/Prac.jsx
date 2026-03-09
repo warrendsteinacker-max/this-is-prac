1bc84b829cb51108fbdbd2e8c13aac321cb65c2c
Warren <warrendsteinacker@gmail.com>
2026-03-09 09:20:35 -0400

    setting up my component for guessing game

--- src/Prac.jsx
+++ src/Prac.jsx
@@ -2,14 +2,23 @@ import {useState} from 'react'
 
 
 const Prac = () => {
+
+    const [G, setG] = useState(0)
+
+    const func
   return (
-
-
+    <>
+    <form onSubmit={}>
+        <input value={G} onChange={() => setG(parseInt(e.target.value))} />
+        <button type='submit'>Start Game</button>
+    </form>
     
+
     <div style={{display: 'grid', gridTemplateColumns: 'repeat(3, 1rf)', gridTemplateRows: 'repeat(3, 1rf)', width: 'fit-content', height: 'fit-content'}}>
 
     </div>
+    </>
   )
 }
 
-export default Pr
+export default Prac
